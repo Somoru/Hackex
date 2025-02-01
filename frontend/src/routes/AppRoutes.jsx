6003281eed5aebf3e3dd28587a3a7bf81506b0b8
Somoru <lakshmipavanrangisetty07@gmail.com>
2025-02-01 13:30:03 +0530

    browserrouter instead of hashrouter

--- frontend/src/routes/AppRoutes.jsx
+++ frontend/src/routes/AppRoutes.jsx
@@ -1,4 +1,4 @@
-import { HashRouter as Router, Routes, Route, Navigate } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
 import Home from "../pages/Home";
 import Signup from "../pages/Signup";
 import Login from "../pages/Login";
