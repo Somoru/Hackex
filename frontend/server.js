cd15c407a201492cde079823b20b2cf82be23718
Somoru <lakshmipavanrangisetty07@gmail.com>
2025-02-01 16:17:52 +0530

    port 8080

--- frontend/server.js
+++ frontend/server.js
@@ -3,7 +3,7 @@ import path from 'path';
 import { fileURLToPath } from 'url';
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8080;
 
 // Fix __dirname in ES Module
 const __filename = fileURLToPath(import.meta.url);
